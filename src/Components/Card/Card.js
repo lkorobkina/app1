d9d49fe643c583d3af89937314ff92a7d799bfaa
lkorobkina <lizakorobkina02@gmail.com>
2022-04-29 11:20:42 +0300

    commit

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,13 +1,27 @@
 import './Card.css';
+import {useState} from "react";
 
 export function Card(props){
+
+    const [checked, setChecked] = useState(true);
+
+    function changeCheckbox() {
+        setChecked(!checked);
+        if (checked) {
+            document.getElementById('caption').style.font = "italic small-caps bold 16px/2 cursive";
+        }
+        else {
+            document.getElementById('caption').style.font = "caption";
+        }
+    }
+
 
     return <div className='bolder'>
             <div className='text'>
 
                 <div className='row'>
-                    <input type="checkbox" id="chetyre"/>
-                    <p>{props.caption}</p>
+                    <input type="checkbox" id="chetyre" onChange={changeCheckbox}/>
+                    <p id="caption">{props.caption}</p>
                 </div>
                     <hr/>
                     <p>{props.text}</p>

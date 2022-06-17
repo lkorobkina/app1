1c4c5ca14dc5d3004aa4c414c75473d233e9c423
lkorobkina <lizakorobkina02@gmail.com>
2022-06-17 11:39:26 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header/Header.js';
 import CardList from './Components/CardList/CardList.js';
 import {cards} from "./Data";
+import styled from "styled-components";
 
 const App = props => {
 
@@ -17,7 +18,15 @@ const App = props => {
             <div>
                 <Header name="HEADER"/>
                 <div className="bolder">
-                    <input className='cb' type="checkbox" onChange={checkboxWatchOnlyHandler}/>Только просмотр
+                    <Styled
+                        onClick={() => checkboxWatchOnlyHandler()}
+                    >
+                        <input
+                            type="checkbox"
+                            checked={isDisableMode}
+                        />
+                        <label>Только просмотр</label>
+                    </Styled>
                     <div className="cards">
                         <CardList items={cards} isDisableMode={isDisableMode}/>
                     </div>
@@ -29,3 +38,49 @@ const App = props => {
 }
 
 export default App;
+
+const Styled = styled.div`
+  display: inline-block;
+
+  > input {
+    opacity: 0;
+  }
+
+  > input + label {
+    position: relative;
+    padding-left: 900px;
+    padding-top: 5px;
+    cursor: pointer;
+
+    &:before {
+      content: '';
+      position: absolute;
+      left: 870px;
+      top: 10px;
+      width: 17px;
+      height: 17px;
+      border: 1px solid #aaa;
+      background: #f8f8f8;
+      border-radius: 3px;
+      box-shadow: inset 0 1px 3px rgba(0, 0, 0, .3)
+    }
+
+    &:after {
+      content: '✔';
+      position: absolute;
+      top: 7px;
+      left: 873px;
+      font-size: 16px;
+      color: #09ad7e;
+      transition: all .2s;
+    }
+  }
+
+  > input:not(:checked) + label {
+    &:after {
+      opacity: 0;
+      transform: scale(0);
+    }
+  }
+
+`;
